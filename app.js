2b677afcd6fe0e01f1ca765e218f5035847c751c
Julia <JLHartsek@gmail.com>
2022-11-06 12:04:39 -0500

    add articles router

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRouter = require('./routes/users');
+const articleRouter = require('./routes/articles');
 
 const app = express();
 const PORT = 3001;
 
 app.use('/users', userRouter);
+app.use('/articles', articleRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
