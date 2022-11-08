be511612be44b10b637c8a39bfdc71c116f574e8
Julia <JLHartsek@gmail.com>
2022-11-08 08:57:30 -0500

    implement request/error logging

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const userRouter = require('./routes/users');
 const articleRouter = require('./routes/articles');
+const { requestLogger, errorLogger } = require('./middleware/logger');
 
 const app = express();
 const PORT = 3001;
 
+app.use(requestLogger);
+
 app.use('/users', userRouter);
 app.use('/articles', articleRouter);
+
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
