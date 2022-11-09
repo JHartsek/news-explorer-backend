77dece335008f572d2a17cbe5c005f5ceb414004
Julia <JLHartsek@gmail.com>
2022-11-09 13:23:36 -0500

    fix: update errorHandler export

--- middleware/errors.js
+++ middleware/errors.js
@@ -1,11 +1,9 @@
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message:
-        statusCode === 500 ? 'An error has occured on the server' : message,
-    });
+  res.status(statusCode).send({
+    message:
+      statusCode === 500 ? 'An error has occured on the server' : message,
+  });
 };
 
-export default errorHandler;
+module.exports = errorHandler;
