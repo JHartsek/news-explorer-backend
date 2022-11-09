522da51d0badf8f7795181ec0a333795ff2d528e
Julia <JLHartsek@gmail.com>
2022-11-09 16:51:07 -0500

    update fields sent in response

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,9 @@ const getCurrentUser = (req, res, next) => {
       res.send(user);
     })
     .catch((err) => {
-      err.name === 'DocumentNotFoundError' ? next(new ResourceNotFoundError('Could not find requested user')) : next(err);
+      err.name === 'DocumentNotFoundError'
+        ? next(new ResourceNotFoundError('Could not find requested user'))
+        : next(err);
     });
 };
 
@@ -26,18 +28,19 @@ const createUser = (req, res, next) => {
   const { name, email, password } = req.body;
   userModel
     .findOne({ email })
-    .select('passord')
+    .select('+password')
     .then((account) => {
       if (account) {
-        return Promise.reject(new ConflictError('This email has already been taken!'));
+        return Promise.reject(
+          new ConflictError('This email has already been taken!'),
+        );
       }
       bcrypt
         .hash(password, 10)
         .then((hash) => {
-          userModel.create({ name, email, password: hash });
-        })
-        .then((user) => {
-          res.send(user);
+          userModel.create({ name, email, password: hash }).then((user) => {
+            res.send({ name, email });
+          });
         })
         .catch((err) => {
           next(err);
@@ -53,24 +56,28 @@ const login = (req, res, next) => {
   let user;
   userModel
     .findOne({ email })
-    .select('password')
+    .select('+password')
     .then((account) => {
       if (!account) {
-        return Promise.reject(new UnauthorizedError('Incorrect email or password'));
+        return Promise.reject(
+          new UnauthorizedError('Incorrect email or password'),
+        );
       }
       user = account;
       return bcrypt.compare(password, user.password);
     })
     .then((authenticated) => {
       if (!authenticated) {
-        return Promise.reject(new UnauthorizedError('Incorrect email or password'));
+        return Promise.reject(
+          new UnauthorizedError('Incorrect email or password'),
+        );
       }
       const token = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'encoding-string',
         { expiresIn: '7d' },
       );
-      res.send({ user, token });
+      res.send({ email: user.email, token });
     })
     .catch((err) => {
       next(err);
