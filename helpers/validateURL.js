3e3190bf38b60ad58fbc36f704213fe9816614fb
Julia <JLHartsek@gmail.com>
2022-11-09 13:47:55 -0500

    add url validation helper

---
+++ helpers/validateURL.js
@@ -0,0 +1,12 @@
+const validator = require('validator');
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error('string.uri');
+};
+
+module.exports = {
+  validateURL,
+};
