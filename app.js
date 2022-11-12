2963a1927643a71688bbb5b6534f40e05c7e5c7c
Julia <JLHartsek@gmail.com>
2022-11-11 19:50:43 -0500

    enable cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { router } = require('./routes/index');
@@ -7,6 +8,8 @@ const { requestLogger, errorLogger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errors');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(helmet());
 mongoose.connect('mongodb://localhost:27017/news');
