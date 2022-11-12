9632d80c9eaea9e1ab74201ec6ae235c013bc5da
Julia <JLHartsek@gmail.com>
2022-11-12 16:39:20 -0500

    add / error

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,17 @@ const userRouter = require('./users');
 const articleRouter = require('./articles');
 const { validateCreateUser, validateLogin } = require('../helpers/validators');
 const { createUser, login } = require('../controllers/users');
+const { ResourceNotFoundError } = require('../errors/ResourceNotFoundError');
 
 router.post('/signup', validateCreateUser, createUser);
 router.post('/signin', validateLogin, login);
 
 router.use('/users', auth, userRouter);
 router.use('/articles', auth, articleRouter);
+
+router.use('/', (req, res) => {
+  throw new ResourceNotFoundError('Requested resource not found');
+});
 
 module.exports = {
   router,
