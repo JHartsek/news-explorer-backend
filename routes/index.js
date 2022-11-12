e1fc3d7ba2dffc66c9ef43c59d93068fcd500a26
Julia <JLHartsek@gmail.com>
2022-11-12 16:50:23 -0500

    update routes

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,12 @@ const userRouter = require('./users');
 const articleRouter = require('./articles');
 const { validateCreateUser, validateLogin } = require('../helpers/validators');
 const { createUser, login } = require('../controllers/users');
-const { ResourceNotFoundError } = require('../errors/ResourceNotFoundError');
 
 router.post('/signup', validateCreateUser, createUser);
 router.post('/signin', validateLogin, login);
 
 router.use('/users', auth, userRouter);
 router.use('/articles', auth, articleRouter);
-
-router.use('/', (req, res) => {
-  throw new ResourceNotFoundError('Requested resource not found');
-});
 
 module.exports = {
   router,
