f4b3cf8911c8edcc7d4cb7c1ed98d43ce499b694
Julia <JLHartsek@gmail.com>
2022-11-13 11:14:23 -0500

    add not found route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const { auth } = require('../middleware/auth');
 const userRouter = require('./users');
 const articleRouter = require('./articles');
+const { ResourceNotFoundError } = require('../errors/ResourceNotFoundError');
 
 router.use('/users', auth, userRouter);
 router.use('/articles', auth, articleRouter);
+
+router.use('/', (req, res) => {
+  throw new ResourceNotFoundError('Path not found');
+});
 
 module.exports = {
   router,
